91b1320ee086eecbb1edf94219dcc22d9c0dcc8b
Kishchuk Mukhailo <118667770+Derewashchka@users.noreply.github.com>
2023-12-01 00:13:58 +0200

    2

--- Script/script11.js
+++ Script/script11.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Мінімальна кількість слів для перекладу
-    var maxWords = 10;
-    // Слова для перекладу випадковим чином
-    var words = [
+    let maxWords = 10;
+    const words = [
         { en: "always", ua: "завжди" },
         { en: "never", ua: "ніколи" },
         { en: "sometimes", ua: "іноді" },
@@ -14,73 +12,44 @@ document.addEventListener('DOMContentLoaded', function () {
         { en: "thank you", ua: "дякую" },
         { en: "sorry", ua: "вибач" }
     ];
-    // Поточне слово
-    var currentWord = words[0];
-    // Поточний крок
-    var currentStep = 1;
-    // Поточна кількість вірно перекладених слів
-    var correctCount = 0;
-    // Поточна кількість невірно перекладених слів
-    var incorrectCount = 0;
-    // Поточний рівень знань мови
-    var level = 0;
+    let currentWord = words[0];
+    let currentStep = 1;
+    let correctCount = 0;
+    let incorrectCount = 0;
+    let level = 0;
 
-    // Функція для перевірки правильності перекладу
     function checkAnswer() {
-        // Отримати введений переклад
-        var input = $(".input").val();
-        // Порівняти з правильним перекладом
-        if (input == currentWord.ua) {
-            // Збільшити кількість вірно перекладених слів
+        const input = $(".input").val();
+        if (input === currentWord.ua) {
             correctCount++;
-            // Оновити статистику
-            $(".stats").text("Вірно: " + correctCount + " Невірно: " + incorrectCount + " Крок: " + currentStep + "/" + maxWords);
-            // Показати повідомлення
+            $(".stats").text(`Вірно: ${correctCount} Невірно: ${incorrectCount} Крок: ${currentStep}/${maxWords}`);
             alert("Вірно!");
         } else {
-            // Збільшити кількість невірно перекладених слів
             incorrectCount++;
-            // Оновити статистику
-            $(".stats").text("Вірно: " + correctCount + " Невірно: " + incorrectCount + " Крок: " + currentStep + "/" + maxWords);
-            // Показати повідомлення
+            $(".stats").text(`Вірно: ${correctCount} Невірно: ${incorrectCount} Крок: ${currentStep}/${maxWords}`);
             alert("Невірно!");
         }
-        // Очистити поле для введення
         $(".input").val("");
-        // Перейти до наступного кроку
         nextStep();
     }
 
-    // Функція для переходу до наступного кроку
     function nextStep() {
-        // Збільшити поточний крок
         currentStep++;
-        // Якщо поточний крок більший за мінімальну кількість слів
         if (currentStep > maxWords) {
-            // Завершити застосунок
             finish();
         } else {
-            // Вибрати наступне слово випадковим чином
             currentWord = words[Math.floor(Math.random() * words.length)];
-            // Оновити картку
             $(".card").text(currentWord.en);
-            // Оновити статистику
-            $(".stats").text("Вірно: " + correctCount + " Невірно: " + incorrectCount + " Крок: " + currentStep + "/" + maxWords);
-            // Оновити прогрес
+            $(".stats").text(`Вірно: ${correctCount} Невірно: ${incorrectCount} Крок: ${currentStep}/${maxWords}`);
             $(".bar").css("width", (currentStep - 1) / maxWords * 100 + "%");
         }
     }
 
-    // Функція для завершення застосунку
     function finish() {
-        // Обчислити рівень знань мови
-        level = correctCount / maxWords * 100;
-        // Оновити модальне вікно
-        $(".modal-content").text("Рівень знань: " + level + "%");
-        // Показати модальне вікно
+        level = Math.round((correctCount / maxWords) * 100);
+        $(".modal-content").text(`Рівень знань: ${level}%`);
         $(".modal").show();
     }
 
-    // Додати обробник події для кнопки
     $(".button").click(checkAnswer);
 });
